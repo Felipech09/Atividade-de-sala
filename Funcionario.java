bb7dd5e4ebfb013e2188e6ce2018b712b2c6004e
Felipe Campos Hipólito <felipecamhipolito@gmail.com>
2025-09-17 13:14:32 -0300

    Update Funcionario.java

--- Funcionario.java
+++ Funcionario.java
@@ -6,6 +6,21 @@ public Funcionario extends Pessoa {
   public Funcionario(String nome, int idade, String cpf,
                      String cargo, String dataAdmissao, double salario) {
     this.cargo = cargo;
-    this.dataAdmissao = dataAdmissao
-                     }
+    this.dataAdmissao = dataAdmissao;
+    this.salario = salario;
+
+    @Override
+      public String mostrarDados() {
+      return //completar aqui
+      }
+
+    public double calcularSalarioAnual(){
+      return salario * 12;
+    }
+
+    public void receberAumento(){
+      double percentual = 0.0;
+      salario += salario * (percentual / 100);
+      }
+    }
 }
