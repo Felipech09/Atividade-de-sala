6b9b8dfaea61bd027c8d0d7c3aaea3548adfc7f7
Felipe Campos Hipólito <felipecamhipolito@gmail.com>
2025-09-17 13:44:19 -0300

    Update Gerente.java

--- Gerente.java
+++ Gerente.java
@@ -1,3 +1,27 @@
 public class Gerente extends Funcionario {
-  
+  private int equipe;
+  private double bonus;
+
+  public Gerente (String nome, int idade, String cpf,
+                 String cargo, String dataAdmissao, double salario,
+                 int equipe, double bonus) {
+    this.equipe = equipe;
+    this.bonus = bonus;
+    }
+  @Override
+  public String mostrarDados(){
+    super.mostrarDados();
+    System.out.println("Equipe " + equipe + " funcionarios");
+    System.out.println("Bônus= R$ " + bonus);
+    return null;
+  }
+
+  public double calcularSalarioComBonus(){
+    double salario = 0.0;
+    return salario + bonus;
+  }
+
+  publlic void gerenciarEquipe(){
+    System.out.println("Gerenciando " + equipe + "funcionarios");
+  }
 }
