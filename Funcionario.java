b9457659a8e36ba562230f713eeede351655cd58
Felipe Campos Hipólito <felipecamhipolito@gmail.com>
2025-09-17 12:55:13 -0300

    Create Funcionario.java

---
+++ Funcionario.java
@@ -0,0 +1,11 @@
+public Funcionario extends Pessoa {
+  private String cargo;
+  private String dataAdmissao;
+  private double salario;
+
+  public Funcionario(String nome, int idade, String cpf,
+                     String cargo, String dataAdmissao, double salario) {
+    this.cargo = cargo;
+    this.dataAdmissao = dataAdmissao
+                     }
+}
