f92ce032e3a0efb2a4f98b42c6ff7cf321897d76
Felipe Campos Hipólito <felipecamhipolito@gmail.com>
2025-09-17 17:55:52 -0300

    Update Funcionario.java

--- Funcionario.java
+++ Funcionario.java
@@ -1,27 +1,30 @@
-public Funcionario extends Pessoa {
-  private String cargo;
-  private String dataAdmissao;
-  private double salario;
+public class Funcionario extends Pessoa {
+    private String cargo;
+    private String dataAdmissao;
+    private double salario;
 
-  public Funcionario(String nome, int idade, String cpf,
-                     String cargo, String dataAdmissao, double salario) {
-    this.cargo = cargo;
-    this.dataAdmissao = dataAdmissao;
-    this.salario = salario;
-
-    @Override
-      public String mostrarDados() {
-      return "Funcionario{" + "nome='" + nome + '\'' + ", idade=" + idade + ", cpf='" + cpf + '\'' + 
-        ", salario=" + salario + ", dataAdmissao='" + dataAdmissao + '\'' + cargo='" + cargo + '\'' + '}'
-      }
-
-    public double calcularSalarioAnual(){
-      return salario * 12;
+    public Funcionario(String nome, int idade, String cpf,
+                       String cargo, String dataAdmissao, double salario) {
+        super(nome, idade, cpf);
+        this.cargo = cargo;
+        this.dataAdmissao = dataAdmissao;
+        this.salario = salario;
     }
 
-    public void receberAumento(){
-      double percentual = 0.0;
-      salario += salario * (percentual / 100);
-      }
+    @Override
+    public void mostrarDados() {
+        super.mostrarDados();
+        System.out.println("Cargo: " + cargo);
+        System.out.println("Data de Admissão: " + dataAdmissao);
+        System.out.println("Salário: R$ " + salario);
+    }
+
+    public double calcularSalarioAnual() {
+        return salario * 12;
+    }
+
+    public void receberAumento(double percentual) {
+        salario += salario * (percentual / 100);
+        System.out.println("Novo salário com aumento: R$ " + salario);
     }
 }
