c7064f5b34f00fd57e0d0f1028d5fc724a556028
Felipe Campos Hipólito <felipecamhipolito@gmail.com>
2025-09-17 17:56:41 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,19 +1,18 @@
 public class Main {
-  publlic static void main (String[] args) {
-     Funcionario f = new Funcionario("André", 20, "123.456.789-00",
-                                     "Dev Senior", "01/01/2025", 6000.00);
+    public static void main(String[] args) {
+        Funcionario f = new Funcionario("André", 20, "123.456.789-00",
+                                        "Dev Senior", "01/01/2025", 6000.00);
 
-    Gerente g = new Gerente("Felipe", 20, "052.633.841-57",
-                            "Gerente de TI", "17/09/2025", 8500.00, 5, 820.00);
+        Gerente g = new Gerente("Felipe", 20, "052.633.841-57",
+                                "Gerente de TI", "17/09/2025", 8500.00, 5, 820.00);
 
-    System.out.println("==Funcinário==");
-    f.mostrarDados();
-    System.out.println("Salário atual: R$ " + f.calcularSalarioAnual());
+        System.out.println("==Funcionário==");
+        f.mostrarDados();
+        System.out.println("Salário anual: R$ " + f.calcularSalarioAnual());
 
-    System.out.println("==Gerente==");
-    g.mostrarDados();
-    System.out.println("Salário bõnus: R$ " + g.calcularSalarioCOmBonus());
-    g.gerenciarEquipe();
-                            
-  }
+        System.out.println("\n==Gerente==");
+        g.mostrarDados();
+        System.out.println("Salário com bônus: R$ " + g.calcularSalarioComBonus());
+        g.gerenciarEquipe();
+    }
 }
