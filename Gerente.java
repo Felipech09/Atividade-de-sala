db854cd7f69b1a35575bac9a30841471e46bcf73
Felipe Campos Hipólito <felipecamhipolito@gmail.com>
2025-09-17 17:56:18 -0300

    Update Gerente.java

--- Gerente.java
+++ Gerente.java
@@ -1,27 +1,31 @@
 public class Gerente extends Funcionario {
-  private int equipe;
-  private double bonus;
+    private int equipe;
+    private double bonus;
 
-  public Gerente (String nome, int idade, String cpf,
-                 String cargo, String dataAdmissao, double salario,
-                 int equipe, double bonus) {
-    this.equipe = equipe;
-    this.bonus = bonus;
+    public Gerente(String nome, int idade, String cpf,
+                   String cargo, String dataAdmissao, double salario,
+                   int equipe, double bonus) {
+        super(nome, idade, cpf, cargo, dataAdmissao, salario);
+        this.equipe = equipe;
+        this.bonus = bonus;
     }
-  @Override
-  public String mostrarDados(){
-    super.mostrarDados();
-    System.out.println("Equipe " + equipe + " funcionarios");
-    System.out.println("Bônus= R$ " + bonus);
-    return null;
-  }
 
-  public double calcularSalarioComBonus(){
-    double salario = 0.0;
-    return salario + bonus;
-  }
+    @Override
+    public void mostrarDados() {
+        super.mostrarDados();
+        System.out.println("Equipe: " + equipe + " funcionários");
+        System.out.println("Bônus: R$ " + bonus);
+    }
 
-  publlic void gerenciarEquipe(){
-    System.out.println("Gerenciando " + equipe + "funcionarios");
-  }
+    public double calcularSalarioComBonus() {
+        // Here, we can't directly access the private 'salario' field from the superclass.
+        // A better approach is to return just the bonus, as the salary is part of the Funcionario class.
+        // For this specific case, it seems the intention was to calculate the total.
+        // We'll fix the Main class to handle this correctly.
+        return super.salario + bonus;
+    }
+
+    public void gerenciarEquipe() {
+        System.out.println("Gerenciando " + equipe + " funcionários");
+    }
 }
